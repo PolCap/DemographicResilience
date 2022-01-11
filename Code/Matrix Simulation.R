02d2ab55bfee81d67a748f370959d4e563cb13fe
PolCap <pcapdevila.pc@gmail.com>
2022-01-11 14:59:57 +0000

    Update Matrix Simulation.R

--- Code/Matrix Simulation.R
+++ Code/Matrix Simulation.R
@@ -1,8 +1,8 @@
 # ---------------------------------------------------------------------------- #
 # - FILE NAME:   MatrixSimulation.R         
 # - DATE:        10/10/2021
-# - DESCRIPTION: Code to randomise matrices and calculate resilience and 
-#                life history traits
+# - DESCRIPTION: Code to simulate different matrix population models and calculate 
+#                resilience and life history traits
 # - AUTHORS:     Pol Capdevila Lanzaco
 # ---------------------------------------------------------------------------- #
 
@@ -46,53 +46,13 @@ dimensions <- range(c(smallandata$Dimension, smallplandata$Dimension))
 
 rm(smallandata, smallantree, smallplandata, smalltree)
 
-# Simulation ----
+# Simulate random matrices #####################################################
 
-# Store a list of values for the different survival and reproductive curves 
+# Random matrices --------------------------------------------------------------
+# Here we simulate matrix population models with random non-negative elements, 
+# keeping the Umat column sums to <= 1 (=survival cannot be higher than 1). 
 
-params <- list(values= seq(3,max(dimensions),1), #Matrix sizes 
-               types =  c(1.2,1.4, 1.6, 1.8, 1.9, 2.1, 2.3, 2.5, 2.7), #Survival curve types
-               exponents = c(5, 10, 15), # Different exponents rerpoductive curve
-               cutoffs = c(0.2, 0.4, 0.6, 0.8), #Reproductive curve cutoffs
-               slopes = c(0.1,1, 2), # Slopes of the curves
-               starting_values = c(0.1,1, 5))
-
-# Make a data.frame of every possible combination of the parameters
-
-all_var <- expand.grid(params)
-
-# Convert that into a list, where each object is 1 row of the data frame
-
-all_var_list <- as.list(as.data.frame(t(all_var)))
-
-# Calculate the survival and reproductive curves
-
-surv_curvs <- lapply(all_var_list, function(x){
-  generate_survival_curve(type = x[2], 
-                          values = x[1], 
-                          exp=x[3])})
-rep_curvs <- lapply(all_var_list, function(x){
-  generate_reproduction_curve(values = x[1], 
-                              cutoff = x[4], 
-                              slope = x[5],
-                              starting_value = x[6])})
-
-# Transform into a data.frame 
-
-surv_data <- ldply(surv_curvs, data.frame)
-rep_data <- ldply(rep_curvs, data.frame)
-
-# Join them 
-
-life_tables <- surv_data %>% cbind(rep_data[-c(1,2)])
-
-# We remove one matrix that gives error with the isPrimitive function
-# 
-# life_tables <- life_tables %>% filter(.id!=c("V51187"))
-
-# Now we convert the curves into matrix population models ###################### 
-
-# make a cluster, defining the number of cores to use
+# We will use a for each loop 
 
 clus  <- makeCluster(detectCores() - 1)
 
@@ -100,86 +60,98 @@ clus  <- makeCluster(detectCores() - 1)
 
 registerDoSNOW(clus)
 
-# Build the Leslie matrices
+# Build random matrices with stasis, progression and retrogression
 
-leslie_matrices <- foreach(i = unique(life_tables$.id),
-                        .combine = "rbind",
-                        .packages = c("tidyverse", "popdemo", "popbio", "Rage")) %dopar% {
-                          x <- life_tables %>% filter(.id==i)
-                          matrix <- build_leslie(survival = x$px, 
-                                                 reproduction = x$reproduction)
-                          if(isErgodic(matrix$matrix_A)){
-                            tryCatch(lam <- lambda(matrix$matrix_A), 
-                                     error=function(e) NA)
-                            tryCatch(GenT <- generation.time(matrix$matrix_A), 
-                                             error=function(e) NA)
-                            tryCatch(Fec <- vitalRates(matrix$matrix_U,
-                                                       matrix$matrix_F)$fec,
-                                            error=function(e) NA)
-                            tryCatch(rupr <- reac(A = matrix$matrix_A,bound="upper"),
-                                     error=function(e) NA)
-                            tryCatch(rlwr<- reac(A = matrix$matrix_A,
-                                                 bound="lower"),
-                                            error=function(e) NA)
-                            tryCatch(xt <- return.time(matrix$matrix_A), 
-                                           error=function(e) NA)
-                            tryCatch(Dimension <- dim(matrix$matrix_A)[1], 
-                                     error=function(e) NA)
-                            
-    }else{
-      lam <- NA
-      GenT <- NA
-      Fec <- NA
-      rupr<- NA
-      rlwr<- NA
-      xt <- NA
-      Dimension <- NA
-    }
-    return(tibble(mat=list(matrix),lam, 
-                      GenT, Fec, rupr, rlwr, xt, Dimension))
-  }
+random <- foreach(i = c(3:max(dimensions)),
+                  .combine = "rbind") %dopar% {
+                    matrices <- replicate(n=100, 
+                                          random_matrices(dimension=i),
+                                          simplify = F)
+                    return(matrices)
+                  }
+# Estimate the life history traits 
 
+random_m <- foreach(i = c(1:length(random)),
+                    .combine = "rbind",
+                    .packages = c("tidyverse", "popdemo", "popbio", "Rage")) %dopar% {
+                      matrix <- random[[i]]
+                      if(isErgodic(matrix$matrix_A)){
+                        tryCatch(lam <- lambda(matrix$matrix_A), 
+                                 error=function(e) NA)
+                        tryCatch(GenT <- generation.time(matrix$matrix_A), 
+                                 error=function(e) NA)
+                        tryCatch(Fec <- vitalRates(matrix$matrix_U,
+                                                   matrix$matrix_F)$fec,
+                                 error=function(e) NA)
+                        tryCatch(rupr <- reac(A = matrix$matrix_A,bound="upper"),
+                                 error=function(e) NA)
+                        tryCatch(rlwr<- reac(A = matrix$matrix_A,
+                                             bound="lower"),
+                                 error=function(e) NA)
+                        tryCatch(xt <- return.time(matrix$matrix_A), 
+                                 error=function(e) NA)
+                        tryCatch(Dimension <- dim(matrix$matrix_A)[1], 
+                                 error=function(e) NA)
+                        
+                      }else{
+                        lam <- NA
+                        GenT <- NA
+                        Fec <- NA
+                        rupr<- NA
+                        rlwr<- NA
+                        xt <- NA
+                        Dimension <- NA
+                      }
+                      return(tibble(mat=list(matrix), lam,
+                                    GenT, Fec, rupr, rlwr, xt, Dimension))
+                    }
 
-# Now for lefkovitch matrices
-lefkovitch_matrices <- foreach(i = unique(life_tables$.id),
-                           .combine = "rbind",
-                           .packages = c("tidyverse", "popdemo", "popbio", "Rage")) %dopar% {
-                             x <- life_tables %>% filter(.id==i)
-                             tryCatch(matrix <-build_lefkovitch(x=x$x,
-                                                       survival = x$px, 
-                                                       reproduction = x$reproduction),
-                                      error=function(e) NA)
-                             if(isErgodic(matrix$matrix_A)){
-                               tryCatch(lam <- lambda(matrix$matrix_A), 
-                                        error=function(e) NA)
-                               tryCatch(GenT <- generation.time(matrix$matrix_A), 
-                                        error=function(e) NA)
-                               tryCatch(Fec <- vitalRates(matrix$matrix_U,
-                                                          matrix$matrix_F)$fec,
-                                        error=function(e) NA)
-                               tryCatch(rupr <- reac(A = matrix$matrix_A,bound="upper"),
-                                        error=function(e) NA)
-                               tryCatch(rlwr<- reac(A = matrix$matrix_A,
-                                                    bound="lower"),
-                                        error=function(e) NA)
-                               tryCatch(xt <- return.time(matrix$matrix_A), 
-                                        error=function(e) NA)
-                               tryCatch(Dimension <- dim(matrix$matrix_A)[1], 
-                                        error=function(e) NA)
-                               
-                             }else{
-                               lam <- NA
-                               GenT <- NA
-                               Fec <- NA
-                               rupr<- NA
-                               rlwr<- NA
-                               xt <- NA
-                               Dimension <- NA
-                             }
-                             return(tibble(mat=list(matrix), lam,
-                                           GenT, Fec, rupr, rlwr, xt, Dimension))
-                           }
+# Build random matrices only with stasis and progression
 
+random_p <- foreach(i = c(3:max(dimensions)),
+                  .combine = "rbind") %dopar% {
+                    matrices <- replicate(n=100, 
+                                          random_matrices(dimension=i, upd=TRUE),
+                                          simplify = F)
+                    return(matrices)
+                  }
+
+# Estimate the life history traits 
+
+random_mp <- foreach(i = c(1:length(random)),
+                    .combine = "rbind",
+                    .packages = c("tidyverse", "popdemo", "popbio", "Rage")) %dopar% {
+                      matrix <- random_p[[i]]
+                      if(isErgodic(matrix$matrix_A)){
+                        tryCatch(lam <- lambda(matrix$matrix_A), 
+                                 error=function(e) NA)
+                        tryCatch(GenT <- generation.time(matrix$matrix_A), 
+                                 error=function(e) NA)
+                        tryCatch(Fec <- vitalRates(matrix$matrix_U,
+                                                   matrix$matrix_F)$fec,
+                                 error=function(e) NA)
+                        tryCatch(rupr <- reac(A = matrix$matrix_A,bound="upper"),
+                                 error=function(e) NA)
+                        tryCatch(rlwr<- reac(A = matrix$matrix_A,
+                                             bound="lower"),
+                                 error=function(e) NA)
+                        tryCatch(xt <- return.time(matrix$matrix_A), 
+                                 error=function(e) NA)
+                        tryCatch(Dimension <- dim(matrix$matrix_A)[1], 
+                                 error=function(e) NA)
+                        
+                      }else{
+                        lam <- NA
+                        GenT <- NA
+                        Fec <- NA
+                        rupr<- NA
+                        rlwr<- NA
+                        xt <- NA
+                        Dimension <- NA
+                      }
+                      return(tibble(mat=list(matrix), lam,
+                                    GenT, Fec, rupr, rlwr, xt, Dimension))
+                    }
 
 #Stop the cluster 
 
@@ -191,25 +163,19 @@ stopCluster(clus)
 leslie_matrices <- leslie_matrices %>% filter(lam>-1.5&lam<1.5)
 lefkovitch_matrices <- lefkovitch_matrices %>% filter(lam>-1.5&lam<1.5)
 
+
 # We now clean the data as we did in the original analyses 
 
-# We set resitance measures to inverse
-
-#Remove infinites
-
-leslie_matrices <- leslie_matrices[!is.infinite(leslie_matrices$GenT),]
-lefkovitch_matrices <- lefkovitch_matrices[!is.infinite(lefkovitch_matrices$GenT),]
-
-#We logtransform the transient data 
-
-leslie_matrices <- leslie_matrices %>% 
+random_m <- random_m %>% 
+  filter(!is.infinite(GenT)) %>% 
   mutate(xt=log(xt+1),
          rupr=log(rupr+1),
          rlwr=abs(log((1 - rlwr)+1)),
          Fec=log(Fec+1),
          GenT=log(GenT+1))
 
-lefkovitch_matrices <- leslie_matrices %>% 
+random_mp <- random_mp %>% 
+  filter(!is.infinite(GenT)) %>% 
   mutate(xt=log(xt+1),
          rupr=log(rupr+1),
          rlwr=abs(log((1 - rlwr)+1)),
@@ -220,5 +186,6 @@ lefkovitch_matrices <- leslie_matrices %>%
 
 setwd(DataPath)
 
-save(lefkovitch_matrices, leslie_matrices, file="SimData.RData")
+save(random_m, 
+     random_mp, file="SimData.RData")
 
